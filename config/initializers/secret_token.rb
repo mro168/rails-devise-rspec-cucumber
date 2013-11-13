e0e04010231d934d96aafb2496b21c6ddf6e85bf
Mario Stefano <me@mario.sg>
2013-11-13 07:33:38 +0700

    initialize rails apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDeviseRspecCucumber::Application.config.secret_key_base = 'c58684fbcad03c5422b86d32cef4fcf7ba3e57b5eaf4dfa63c227af3185edf98e6c6c7d564256255d138144d7d1b1d4532dbe7dc5d2363404b2889e8b2bee978'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDeviseRspecCucumber::Application.config.session_store :cookie_store, key: '_rails-devise-rspec-cucumber_session'
