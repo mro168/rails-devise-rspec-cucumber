12f906bd5e5378d6f4264892efab3c7663c6fdc6
Mario Stefano <me@mario.sg>
2013-11-13 09:17:31 +0700

    add user validation for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+         
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, case_sensitive: false
 end
